ec3d3f0e35f7e414f2410e9c2a3f8f0471bb842c
Edwin Xu <edwinhuish@gmail.com>
2022-07-09 07:21:10 +0000

    3.7 优雅的重启服务

--- main.go
+++ main.go
@@ -1,24 +1,49 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
-
-	"github.com/edwinhuish/go-gin-example/routers"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/edwinhuish/go-gin-example/pkg/setting"
+	"github.com/edwinhuish/go-gin-example/routers"
 )
 
 func main() {
 	router := routers.InitRouter()
 
-    s := &http.Server{
-        Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
-        Handler:        router,
-        ReadTimeout:    setting.ReadTimeout,
-        WriteTimeout:   setting.WriteTimeout,
-        MaxHeaderBytes: 1 << 20,
-    }
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
+		Handler:        router,
+		ReadTimeout:    setting.ReadTimeout,
+		WriteTimeout:   setting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
 
-	s.ListenAndServe()
+	go func() {
+		if err := s.ListenAndServe(); err != nil {
+			log.Printf("Listen: %s\n", err)
+		}
+	}()
+
+	// 监听中断命令，有中断命令才会往下执行
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	// 已有中断命令，向下执行
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	// shutdown中断会在任务执行完毕后才中断，避免升级过程中，用户进程中途被中断
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+
+	log.Println("Server exiting")
 }
